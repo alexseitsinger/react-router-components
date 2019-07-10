fc273227ad9f0cc4bedb6988327828f204b920b5
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-10 00:15:48 -0400

    feat: Adds `setFullPath` method.

--- src/createRouteComponent/generateRoutes.js
+++ src/createRouteComponent/generateRoutes.js
@@ -1,27 +1,31 @@
 import React from "react"
 
 import { updateRoutes } from "./updateRoutes"
+import { setFullPath } from "./utils"
 
 export function generateRoutes({ config, rootProps, Route }){
   // Sort our route into two arrays.
   const mainRoutes = []
   const modalRoutes = []
 
+  // Set the parents on each config object so we can build the paths easily.
+  setFullPath("/", config)
+
   // Create the routes for the config.
-	updateRoutes({ config, rootProps, mainRoutes, modalRoutes, Route })
+  updateRoutes({ config, rootProps, mainRoutes, modalRoutes, Route })
 
   // Remove the wildcard route to the bottom of the list.
-	mainRoutes.forEach((route, i) => {
+  mainRoutes.forEach((route, i) => {
     if(route.props.path === "*"){
-			mainRoutes.push(mainRoutes.splice(i, 1)[0])
-		}
+      mainRoutes.push(mainRoutes.splice(i, 1)[0])
+    }
   })
 
   // Return them for use in the composed component.
-	return {
-		mainRoutes: mainRoutes,
-		modalRoutes: modalRoutes,
-	}
+  return {
+    mainRoutes,
+    modalRoutes,
+  }
 }
 
 
