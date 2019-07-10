7836de783c0445df834cc3da5b374e52025cd6ec
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-10 00:14:31 -0400

    style: Cleans up `reportRoutes`.

--- src/createRouteComponent/reportRoutes.js
+++ src/createRouteComponent/reportRoutes.js
@@ -1,36 +1,32 @@
 import _ from "underscore"
 
 export function reportRoutes(report, mainRoutes, modalRoutes){
-  const isNotProduction = (process.env.NODE_ENV !== "production")
+  const mainRoutePaths = mainRoutes.map((route, i) => {
+    const number = (i + 1)
+    const path = route.props.path
+    const exact = route.props.exact ? route.props.exact : false
+    return `${number}) ${path}, ${exact}`
+  }).join("\n  ")
 
-  if(isNotProduction) {
-    const mainRoutePaths = mainRoutes.map((route, i) => {
-      const number = (i + 1)
-      const path = route.props.path
-      const exact = route.props.exact ? route.props.exact : false
-      return `${number}) ${path}, ${exact}`
-    }).join("\n  ")
+  const modalRoutePaths = modalRoutes.map((route, i) => {
+    const number = (i + 1)
+    const path = route.props.path
+    const exact = route.props.exact ? route.props.exact : false
+    return `${number}) ${path}, ${exact}`
+  }).join("\n  ")
 
-    const modalRoutePaths = modalRoutes.map((route, i) => {
-      const number = (i + 1)
-      const path = route.props.path
-      const exact = route.props.exact ? route.props.exact : false
-      return `${number}) ${path}, ${exact}`
-    }).join("\n  ")
+  const message = [
+    "[react-router-components]",
+    "Main Routes",
+    ("  " + (mainRoutePaths.length ? mainRoutePaths : "None")),
+    "Modal Routes",
+    ("  " + (modalRoutePaths.length ? modalRoutePaths : "None")),
+  ].join("\n")
 
-    const message = [
-      "[react-router-components]",
-      "Main Routes",
-      ("  " + (mainRoutePaths.length ? mainRoutePaths : "None")),
-      "Modal Routes",
-      ("  " + (modalRoutePaths.length ? modalRoutePaths : "None")),
-    ].join("\n")
-
-    if (report === true) {
-      console.log(message)
-    }
-    else if (_.isFunction(report)){
-      report(message)
-    }
+  if (report === true) {
+    console.log(message)
+  }
+  else if (_.isFunction(report)){
+    report(message)
   }
 }
