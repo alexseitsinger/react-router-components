08bf7515e89dc5f572216810d30af0bbaf2e5f46
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-10 00:13:34 -0400

    refactor: Str concat to template literal.

--- src/createRouteComponent/composeRoute.js
+++ src/createRouteComponent/composeRoute.js
@@ -4,10 +4,10 @@ import _ from "underscore"
 export function composeRoute({ path, Component, rootProps, exact = true, Route }) {
   return (
     <Route
-      key={"route" + _.uniqueId()}
-      path={path} 
+      key={`route${_.uniqueId()}`}
+      path={path}
       exact={exact}
-      render={(routeProps) => <Component {...rootProps} {...routeProps} />} 
-    />  
+      render={(routeProps) => <Component {...rootProps} {...routeProps} />}
+    />
   )
 }
