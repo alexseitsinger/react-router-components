a9ef5d32b8eaae3c0d592a35744e12844cfddce7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-08-01 00:59:19 -0400

    test: Updates tests for second switch.

--- tests/ModalSwitch.test.js
+++ tests/ModalSwitch.test.js
@@ -20,21 +20,46 @@ const IndexPage = () => <div>Index</div>
 const AboutPage = () => <div>About</div>
 const AboutAlexPage = () => <div>AboutAlex</div>
 const NotFoundPage = () => <div>NotFound</div>
+const TestModalOnePage = () => <div>Test Modal Page #1</div>
+const TestModalTwoPage = () => <div>Test Modal Page #2</div>
 
 const config = {
   path: "/",
   Component: IndexPage,
   routes: [
     {path: "about", Component: AboutPage, routes: [
-      {path: "alex", Component: AboutAlexPage, modal: true},
+      {path: "alex", Component: AboutAlexPage, routes: [
+        {path: "test/modal", Component: TestModalTwoPage, modal: true},
+        {path: "test/:uuid", Component: TestModalOnePage, modal: true},
+      ]},
     ]},
     {path: "*", Component: NotFoundPage},
   ]
 }
 
 describe("<ModalSwitch />", () => {
-  it("renders", () => {
+  test("renders index page", () => {
     const wrapper = setup("/", config)
     expect(wrapper.find(IndexPage)).toHaveLength(1)
+  })
+  test("renders the correct route, even when there are additional nested routes", () => {
+    const wrapper = setup("/about/alex", config)
+    expect(wrapper.find(AboutAlexPage)).toHaveLength(1)
+    expect(wrapper.find(AboutPage)).toHaveLength(0)
+    expect(wrapper.find(TestModalTwoPage)).toHaveLength(0)
+    expect(wrapper.find(TestModalOnePage)).toHaveLength(0)
+    expect(wrapper.find(NotFoundPage)).toHaveLength(0)
+  })
+  test("renders only one modal when using keywords and a static url path with a common prefix", () => {
+    const wrapper = setup("/about/alex/test/keyword", config)
+    expect(wrapper.find(AboutAlexPage)).toHaveLength(1)
+    expect(wrapper.find(TestModalOnePage)).toHaveLength(1)
+    expect(wrapper.find(TestModalTwoPage)).toHaveLength(0)
+  })
+  test("renders the correct route static path route even with a keyword route that share a common prefix", () => {
+    const wrapper = setup("/about/alex/test/modal", config)
+    expect(wrapper.find(AboutAlexPage)).toHaveLength(1)
+    expect(wrapper.find(TestModalOnePage)).toHaveLength(0)
+    expect(wrapper.find(TestModalTwoPage)).toHaveLength(1)
   })
 })
